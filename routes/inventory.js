e5fe342c972b32c170c81ae397c6be65f6bde595
uxama-jamil <usama_jamil@hotmail.com>
2022-03-23 15:55:43 +0500

    txt files check

--- routes/inventory.js
+++
@@ -1,7 +0,0 @@
-const express =require('express')
-const route = express.Router()
-const getProduct = require('../controller/product')
-
-route.get('/get-all-product',getProduct.getAllProduct)
-
-module.exports=route;
--- utill/database.js
+++
@@ -1,9 +0,0 @@
-const mysql2 = require("mysql2");
-const pool = mysql2.createPool({
-    host : "localhost",
-    port : 3306,
-    database : "node",
-    user : "root",
-    password : "password"
-})
-module.exports = pool.promise();
